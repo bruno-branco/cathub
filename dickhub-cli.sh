07cf130fdbda8027bb4765f7a6a5d120175db632
Bruno Branco <brunocbranco16@gmail.com>
2025-03-21 14:07:43 -0300

    fix

--- dickhub-cli.sh
+++ dickhub-cli.sh
@@ -17,11 +17,9 @@ curl -s "https://bananasfamosas.com.br/wp-content/uploads/2022/05/50-imagens-cas
 curl -s "https://ei.phncdn.com/videos/202303/13/427289951/thumbs_10/(m=eaAaGwObaaaa)(mh=bDtZgofEf5YoSp04)15.jpg" -o "$TARGET_DIR/dick10.jpg"
 curl -s "https://homenspelados.blog/wp-content/uploads/2023/07/40-fotos-de-pica-1160.jpeg" -o "$TARGET_DIR/dick11.jpg"
 
-# Pick a random image (using sort -R instead of shuf)
-IMAGE=$(ls "$TARGET_DIR" | sort -R | head -n 1)
 
 # Set it as the wallpaper (macOS version with proper quoting)
-osascript -e "tell application \"System Events\" to set picture of every desktop to \"$TARGET_DIR/cat1.jpg\""
+osascript -e "tell application \"System Events\" to set picture of every desktop to \"$TARGET_DIR/dick1.jpg\""
 
 echo "🔥 gosta de queen? entao MAMAAAAA 🔥"
 
